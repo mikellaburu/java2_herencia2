e245dccc0e2fe76aa426ff932ea97e02df463911
mikellaburu <zubiri@Ubuntu-12-04.(none)>
2015-01-15 09:45:29 +0100

    herencia 2

---
+++ src/ProgramaLibro.java
@@ -0,0 +1,23 @@
+public class ProgramaLibro {
+
+	public static void main (String [ ] Args) {
+
+	        Novela novela  = new Novela();
+	        novela.setTipoLibro("Novela");
+	        novela.setPaginas(156);
+	        novela.setISBN("131-46-bad-4v");
+	        novela.setNombre("Los vecinos mueren en las novelas");
+	        novela.setEditorial("Norma");
+	        novela.setTipoNovela("Negra");
+	        novela.setArgumento("pinhńívnǹimi vfasbn vfasojjb vaqfjnñln avñfjbnñbn.");
+	        novela.setCapitulos(12);
+	        
+	        
+	        System.out.println("Tipo de espacio: " + parque.getTipoEspacio());
+	        System.out.println("Ubicacion: " + parque.getUbicacion());
+	        System.out.println("Extension: " + parque.getExtension() + " m2");
+	        System.out.println("Tipo de parque: " + parque.getTipoParque());
+	        System.out.println();
+	        
+	}
+}
